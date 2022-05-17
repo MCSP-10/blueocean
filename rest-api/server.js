a99dd3251c891072416b99f96275363637a32559
hunterAlbertJ <albert.hunter13@gmail.com>
2022-05-17 11:22:19 -0700

    tests started

--- rest-api/server.js
+++ rest-api/server.js
@@ -1,20 +1,8 @@
-import express from 'express';
-import cors from 'cors';
-import * as routes from './routes/index.js';
+import app from "./app.js"
 
-const app = express();
 const PORT = process.env.PORT || 3001;
 
-const corsConfig = {
-    origin: '*',
-};
 
-app.use(cors(corsConfig));
-
-app.use('/users', routes.users);
-app.use('/applications', routes.applications);
-app.use('/groups', routes.groups);
-app.use('/opportunities', routes.opportunities);
 
 app.listen(PORT, () => {
     console.log(`server running on port: ${PORT}`);
